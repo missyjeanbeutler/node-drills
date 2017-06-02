d2036c43fc9b4fba1cfe10c311138ed0fa2baa9c
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-06-02 10:21:31 -0600

    prep

--- app5-queries_params/index.js
+++ app5-queries_params/index.js
@@ -1,13 +1,54 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var port = 3000;
-var data = require('./data.js')
+const express = require('express'),
+			bodyParser = require('body-parser'),
+			port = 3000,
+			data = require('./data')
 
-var app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
+app.get('/api/data', function(req, res){
+	let q = req.query;
 
+	let filteredData = data.filter(function(obj) {
+		for(var key in q) {
+		  if(obj[key] !== q[key]) {
+				return false;
+			}
+		}
+
+		return true;
+	});
+
+	res.status(200).json(filteredData);
+})
+
+app.post('/api/data', function(req, res){
+	var person = req.body
+	data.push(person)
+	res.status(200).send('ok')
+})
+
+app.get('/api/data/:id', function(req, res){
+	let id = req.params.id;
+	res.status(200).json(data[id])
+})
+
+app.delete('/api/data/:id', function(req, res) {
+  var removed = data.splice(req.params.id, 1);
+	res.status(200).send(removed[0]);
+});
+
+app.put('/api/data/:id', function(req, res) {
+	var i = req.params.id;
+	var q = req.query;
+
+	for(var key in q) {
+	  data[i][key] = q[key];
+	}
+
+	res.status(200).send(data[i]);
+})
 
 
 app.listen(port, function() {
